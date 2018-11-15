eb3b1edb3969ac7a707131af40c3cdfa55796a49
ducvu212 <minhducvu.212@gmail.com>
2018-11-15 07:36:42 +0700

    read json which return from server

---
+++ client/LTM/src/ltm/ClientDetails.java
@@ -0,0 +1,17 @@
+package ltm;
+
+public class ClientDetails {
+    private String ip;
+
+    public ClientDetails(String ip) {
+        this.ip = ip;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
