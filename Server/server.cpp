086481250019a0f14d912b0ab74019a2752ecaf0
quangvm97 <robinhood19101997@gmail.com>
2018-11-17 11:11:09 +0700

    + test send json to client success

--- Server/server.cpp
+++ Server/server.cpp
@@ -13,21 +13,24 @@
 #include <vector>
 #include <cstring>
 #include <arpa/inet.h>
+#include <jsoncpp/json/json.h>
 
 //#define _BSD_SOURCE
-#define PORT 4000
+#define PORT 8080
 #define SIZE_BUFFER 1024
 
 using namespace std;
-class Client{
-    public:
+class Client
+{
+public:
     int id;
     std::string ip;
     int port;
 };
 
-class File{
-    public:
+class File
+{
+public:
     std::string name;
     int client_id;
 };
@@ -41,17 +44,18 @@ void register_client(string ip, int port, int id)
     new_client.port = port;
     new_client.id = id;
     clients.push_back(new_client);
-
 }
 
-void convert(char*  buffer, int id){
+void convert(char* buffer, int id)
+{
     char str[4000];
-    memset(&str,'\0',sizeof(str));
-    strcpy(str,buffer);
-    memset(&buffer,'\0',sizeof(buffer));
-    char *p = strtok(str, " ");
-    //get name file
-    while(p){
+    memset(&str, '\0', sizeof(str));
+    strcpy(str, buffer);
+    memset(&buffer, '\0', sizeof(buffer));
+    char* p = strtok(str, " ");
+    // get name file
+    while (p)
+    {
         string tmp = p;
         File new_file;
         new_file.name = tmp;
@@ -75,7 +79,7 @@ int main(int argc, char const* argv[])
     fd_set rfds, afds;
     int connfd;
     int id = 0;
-    char *status = "1"; 
+    char* status = "1";
     char hello = '1';
 
     // Creating socket file descriptor
@@ -110,47 +114,59 @@ int main(int argc, char const* argv[])
     printf("\nlistening...\n");
     // FD_ZERO(&afds);
     // FD_SET(server_fd, &afds);
- 
-  
-            if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen))
-                < 0)
-            {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
-            struct sockaddr_in* sock_client = (struct sockaddr_in*)&address;
 
-            char str_cli_ip[INET_ADDRSTRLEN];
-            char str_cli_port[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &sock_client->sin_addr, str_cli_ip, INET_ADDRSTRLEN);
-            printf("\nclient ip: %s", str_cli_ip);
-            printf("\nclient port: %d\n", ntohs(sock_client->sin_port));
-            fflush(stdout);
-            // FD_SET(new_socket, &afds);
-            register_client(str_cli_ip,ntohs(sock_client->sin_port), id);
-            id++;
-            send(new_socket , status , strlen(status) , 0 );
-            //Gui thong bao ok
-             fflush(stdout);
-                            //Nhan danh sach file tu client
-                            memset(&buffer,'\0',sizeof(buffer));
-                            valread = read( new_socket , buffer, 1024);
-                            //Neu co tra ve danh sach file thi se luu vao
-                            if(strcmp(buffer, "") != 0){
-                                convert(buffer, id);
-                            }
 
-                            int count = files.size();
-                            cout << "Tong so file: " << count;
-                            cout << "Danh sach file la: ";
-                            for(int i = 0; i < count; i++){
-                                cout << (files.at(i).name) << " " ;
-                            }
-                            // cout << clients.size();
-                            fflush(stdout);
-            
-        
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    struct sockaddr_in* sock_client = (struct sockaddr_in*)&address;
 
-    
+    char str_cli_ip[INET_ADDRSTRLEN];
+    char str_cli_port[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &sock_client->sin_addr, str_cli_ip, INET_ADDRSTRLEN);
+    printf("\nclient ip: %s", str_cli_ip);
+    printf("\nclient port: %d\n", ntohs(sock_client->sin_port));
+    fflush(stdout);
+    // FD_SET(new_socket, &afds);
+    register_client(str_cli_ip, ntohs(sock_client->sin_port), id);
+    id++;
+    send(new_socket, status, strlen(status), 0);
+    // Gui thong bao ok
+    fflush(stdout);
+    // Nhan danh sach file tu client
+    memset(&buffer, '\0', sizeof(buffer));
+    valread = read(new_socket, buffer, 1024);
+    // Neu co tra ve danh sach file thi se luu vao
+    if (strcmp(buffer, "") != 0)
+    {
+        convert(buffer, id);
+    }
+
+    int count = files.size();
+    cout << "Tong so file: " << count;
+    cout << "Danh sach file la: ";
+    for (int i = 0; i < count; i++)
+    {
+        cout << (files.at(i).name) << " ";
+    }
+    Json::Value ch;
+    ch[0]["ip"] = "127.0.0.1";
+    ch[0]["port"] = 12;
+    ch[1]["ip"] = "127.0.0.2";
+    ch[1]["port"] = 134;
+
+    Json::Value val;
+    val["list"] = ch;
+    Json::FastWriter fast;
+    string sFast = fast.write(val);
+    send(new_socket, sFast.data(), sFast.size(), 0);
+    cout<<sFast;
+
+    // cout << clients.size();
+    fflush(stdout);
+
+
     return 0;
 }
